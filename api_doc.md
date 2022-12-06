a2978167d0b6b36bde91035c7147264e144ff68d
Suma Gayathri <104271789+SumaGayathri-999@users.noreply.github.com>
2022-12-06 19:55:36 +0530

    Add files via upload

---
+++ api_doc.md
@@ -0,0 +1,43 @@
+
+//list of products
+http://localhost:8400/products
+
+//list of products wrt category id
+http://localhost:8400/categories/1
+
+//list of products wrt product id
+http://localhost:8400/products/2
+
+//list of products wrt concern id
+http://localhost:8400/concern/2
+
+//list of products wrt ingredient id
+http://localhost:8400/ingredients/2
+
+//list of products wrt category and cost(filter)
+http://localhost:8400/categories/1?min_price=500&max_price=1000
+
+//list of products wrt ingredient and cost(filter)
+http://localhost:8400/ingredients/2?min_price=200&max_price=300
+
+//list of products wrt concern and cost(filter)
+http://localhost:8400/concern/2?min_price=200&max_price=300
+
+//list of products wrt category and product type id(filter)
+http://localhost:8400/categories/1?product_type_id=2
+
+//list of products wrt ingredient and product type id(filter)
+http://localhost:8400/ingredients/2?product_type_id=4
+
+
+//list of products wrt concern and product type id(filter)
+http://localhost:8400/concern/2?product_type_id=3
+
+//list of products wrt category and cost and product type id(filter)
+http://localhost:8400/categories/1?min_price=500&max_price=1000&product_type_id=2
+
+//list of products wrt ingredient and cost and product type id(filter)
+http://localhost:8400/ingredients/2?min_price=100&max_price=500&product_type_id=1
+
+//list of products wrt concern and cost and product type id(filter)
+http://localhost:8400/concern/2?min_price=200&max_price=300&product_type_id=1
---
+++ index.js
@@ -0,0 +1,116 @@
+let express = require('express')
+let app = express();
+let dotenv = require('dotenv');
+dotenv.config();
+let port = process.env.PORT || 3000;
+let mongo = require('mongodb');
+let MongoClient = mongo.MongoClient;
+let mongourl = process.env.LiveMongo;
+let db;
+
+app.get("/products",(req,res)=>{
+
+    db.collection("products").find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+})
+
+
+app.get("/products/:productid",(req,res)=>{
+    let productid=Number(req.params.productid);
+    db.collection("products").find({"product_id":productid}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+    
+})
+
+
+app.get("/categories/:categoryid",(req,res)=>{
+    let categoryid = Number(req.params.categoryid);
+    let min_price = Number(req.query.min_price);
+    let max_price = Number(req.query.max_price);
+    let product_type_id = Number(req.query.product_type_id);
+    let query ={};
+    
+    if(min_price && max_price && product_type_id){
+        query = {$and:[{"quantity.price":{$gt:min_price}},{"quantity.price":{$lt:max_price}},{"category_id":{$in:[categoryid]}},{"product_type_id":product_type_id}]};
+    }
+    else if(min_price && max_price){
+         query = {$and:[{"quantity.price":{$gt:min_price}},{"quantity.price":{$lt:max_price}},{"category_id":{$in:[categoryid]}}]};
+    }
+    else if(product_type_id){
+         query={$and:[{"product_type_id":product_type_id},{"category_id":{$in:[categoryid]}}]}
+    }
+    else{
+         query={"category_id":{$in:[categoryid]}};
+    }
+    
+    db.collection("products").find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+})
+app.get("/ingredients/:ingredientid",(req,res)=>{
+    let ingredientid = Number(req.params.ingredientid);
+    let min_price = Number(req.query.min_price);
+    let max_price = Number(req.query.max_price);
+    let product_type_id = Number(req.query.product_type_id);
+    let query={};
+    if(min_price && max_price && product_type_id){
+        query = {$and:[{"quantity.price":{$gt:min_price}},{"quantity.price":{$lt:max_price}},{"ingredient_id":ingredientid},{"product_type_id":product_type_id}]};
+    }
+    else if(min_price && max_price){
+         query = {$and:[{"quantity.price":{$gt:min_price}},{"quantity.price":{$lt:max_price}},{"ingredient_id":ingredientid}]};
+    }
+    else if(product_type_id){
+         query={$and:[{"product_type_id":product_type_id},{"ingredient_id":ingredientid}]}
+    }
+    else{
+         query={"ingredient_id":ingredientid};
+    }
+    
+    db.collection("products").find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+    
+})
+app.get("/concern/:concernid",(req,res)=>{
+    let concernid = Number(req.params.concernid);
+    let min_price = Number(req.query.min_price);
+    let max_price = Number(req.query.max_price);
+    let product_type_id = Number(req.query.product_type_id);
+    let query ={};
+    if(min_price && max_price){
+        query = {$and:[{"quantity.price":{$gt:min_price}},{"quantity.price":{$lt:max_price}},{"concern_id":{$in:[concernid]}},{"product_type_id":product_type_id}]};
+
+    }
+    else if(min_price && max_price){
+         query = {$and:[{"quantity.price":{$gt:min_price}},{"quantity.price":{$lt:max_price}},{"concern_id":{$in:[concernid]}}]};
+    }
+    else if(product_type_id){
+         query={$and:[{"product_type_id":product_type_id},{"concern_id":{$in:[concernid]}}]}
+    }
+    else{
+         query={"concern_id":{$in:[concernid]}};
+    }
+    
+    db.collection("products").find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+})
+
+MongoClient.connect(mongourl,(err,client)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        db=client.db("wow_data");
+        app.listen(port,()=>{
+            console.log("lisening")
+        })
+    }
+})
