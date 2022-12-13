46ae4e531c98a38d1b75d042c36d47a6751db5ec
Suma Gayathri <104271789+SumaGayathri-999@users.noreply.github.com>
2022-12-13 13:41:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ let db;
 const cors = require("cors");
 app.use(cors());
 
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Credentials",true);
+    res.header("Access-Control-Allow-Methods",'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers",'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.get("/products",(req,res)=>{
 
     db.collection("products").find().toArray((err,result)=>{
