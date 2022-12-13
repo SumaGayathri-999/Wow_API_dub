c1a90d99a26e440e50a9b98f496695e9460af669
Suma Gayathri <104271789+SumaGayathri-999@users.noreply.github.com>
2022-12-13 15:09:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ let mongourl = process.env.LiveMongo;
 let db;
 const cors = require("cors");
 app.use(cors());
-
 app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Credentials",true);
-    res.header("Access-Control-Allow-Methods",'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers",'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Credentials",true);
+    res.setHeader("Access-Control-Allow-Methods",'GET,PUT,POST,DELETE,OPTIONS');
+    res.setHeader("Access-Control-Allow-Headers",'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
-});
+})
 
 app.get("/products",(req,res)=>{
 
